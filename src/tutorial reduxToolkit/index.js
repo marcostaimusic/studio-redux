bb09044493f5608b704efe6262e431b9c3875b23
casa <marco.zappani@gmail.com>
2022-09-03 16:42:23 +0200

    redux toolkit 20

--- src/tutorial reduxToolkit/index.js
+++ src/tutorial reduxToolkit/index.js
@@ -1,3 +1,15 @@
 const store = require('./App/store')
-
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
 console.log(store.getState());
+
+const unsubscribe = store.subscribe(()=>{
+    console.log('updated', store.getState());
+})
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(12))
+
+
+unsubscribe()
